c8448e9eac69968ee17ec4583b8cf3ab189a0fcd
AntoineCharette <antoineocharette@gmail.com>
2021-11-04 14:04:14 -0400

    adds url to token server

--- www/client.js
+++ www/client.js
@@ -1,10 +1,22 @@
-
 /**  Create a list of Cities for our Money Heist Characters */
-const cities = ['Seattle', 'Vancouver', 'Portland', 'Tokyo', 'Berlin', 'Palermo', 'Nairobi', 'Denver', 'Helsinki', 'Rio', 'Moscow', 'Oslo'];
+const cities = [
+  "Seattle",
+  "Vancouver",
+  "Portland",
+  "Tokyo",
+  "Berlin",
+  "Palermo",
+  "Nairobi",
+  "Denver",
+  "Helsinki",
+  "Rio",
+  "Moscow",
+  "Oslo",
+];
 let randomName = cities[Math.floor(Math.random() * cities.length)];
 
 // We'll pulls some images that have been named after cities... {Money Heist} and replace our city name in URL and add some cloudinary transformations.
-let imagePlaceholder = `https://res.cloudinary.com/dolby-io/image/upload/e_art:red_rock/ar_1:1,c_fill,g_auto,r_max,w_50/v1634690310/dolby-hackathon/cities/${randomName}.png`
+let imagePlaceholder = `https://res.cloudinary.com/dolby-io/image/upload/e_art:red_rock/ar_1:1,c_fill,g_auto,r_max,w_50/v1634690310/dolby-hackathon/cities/${randomName}.png`;
 
 /**  Update varibles when form input changes */
 function updateNameValue(e) {
@@ -12,16 +24,14 @@ function updateNameValue(e) {
 }
 
 // URL to our Token Server
-const tokenServerURL = 'Enter the url to your token server here';
-
-
+const tokenServerURL = "https://dolby-workshop-token-server.netlify.app/";
 
 /**   initializeToken authorization flow on script load  **/
 (function () {
   try {
-    getTokenAndInitalize()
+    getTokenAndInitalize();
   } catch (e) {
-    alert('Something went wrong initalizaton : ' + e);
+    alert("Something went wrong initalizaton : " + e);
   }
 })();
 
@@ -33,11 +43,11 @@ async function getTokenAndInitalize() {
     })
     .then((result) => {
       VoxeetSDK.initializeToken(result.access_token, refreshToken);
-      return result.access_token
+      return result.access_token;
     })
     .then((token) => {
-      console.info('token received', token);
-      initializeConferenceSession()
+      console.info("token received", token);
+      initializeConferenceSession();
     })
     .catch((error) => {
       console.error(error);
@@ -58,23 +68,27 @@ async function refreshToken() {
 
 /**  Create the participantInfo object and open the session with the object  */
 async function initializeConferenceSession() {
-  let participantInfo = { name: randomName, avatarUrl: imagePlaceholder, externalId: randomName }
+  let participantInfo = {
+    name: randomName,
+    avatarUrl: imagePlaceholder,
+    externalId: randomName,
+  };
   try {
     // Open a session for the user
     await VoxeetSDK.session.open(participantInfo);
     // Initialize the UI
     initUI();
-    console.log('session initialized!');
+    console.log("session initialized!");
   } catch (e) {
-    alert('Something went wrong: ' + e);
+    alert("Something went wrong: " + e);
   }
 }
 
 /* Dolby.io Event handlers */
 
 // When a stream is added to the conference
-VoxeetSDK.conference.on('streamAdded', (participant, stream) => {
-  if (stream.type === 'ScreenShare') {
+VoxeetSDK.conference.on("streamAdded", (participant, stream) => {
+  if (stream.type === "ScreenShare") {
     return addScreenShareNode(stream);
   }
   if (stream.getVideoTracks().length) {
@@ -85,8 +99,8 @@ VoxeetSDK.conference.on('streamAdded', (participant, stream) => {
 });
 
 // When a stream is updated
-VoxeetSDK.conference.on('streamUpdated', (participant, stream) => {
-  if (stream.type === 'ScreenShare') return;
+VoxeetSDK.conference.on("streamUpdated", (participant, stream) => {
+  if (stream.type === "ScreenShare") return;
   if (stream.getVideoTracks().length) {
     // Only add the video node if there is a video track
     addVideoNode(participant, stream);
@@ -96,8 +110,8 @@ VoxeetSDK.conference.on('streamUpdated', (participant, stream) => {
 });
 
 // When a stream is removed from the conference
-VoxeetSDK.conference.on('streamRemoved', (participant, stream) => {
-  if (stream.type === 'ScreenShare') {
+VoxeetSDK.conference.on("streamRemoved", (participant, stream) => {
+  if (stream.type === "ScreenShare") {
     return removeScreenShareNode();
   }
   removeVideoNode(participant);
